d0bbba7a4b9756aad7d6693b76a989ed2b42a772
peri-carvalho <pericles.carvalho@precojusto.app.br>
2024-10-04 10:48:28 -0400

    Seção 4 Curso Angular

--- udemy-angular-course/src/app/app.component.ts
+++ udemy-angular-course/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeiroComponente } from "./components/primeiro-componente/primeiro-componente.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, PrimeiroComponente],
+  template: ` 
+  <!-- <router-outlet></router-outlet> -->
+  <h1>Curso de Angular</h1>
+  <h2>Components</h2>
+  {{title}}
+  <app-primeiro-componente/>
+  `,
 })
 export class AppComponent {
   title = 'udemy-angular-course';
